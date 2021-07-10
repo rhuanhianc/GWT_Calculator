19df5501ec6f718720dbbbf527d35427898d13ea
Rhuan <rhuan.hianc@el.com.br>
2021-07-10 18:43:44 -0300

    corrigido bugs e aprimorado visual

--- src/com/calc/client/CalculadoraTeste.java
+++ src/com/calc/client/CalculadoraTeste.java
@@ -1,116 +1,250 @@
 package com.calc.client;
 
-import com.calc.shared.FieldVerifier;
+
+
+
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HasAlignment;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.HasVerticalAlignment.VerticalAlignmentConstant;
+
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class CalculadoraTeste implements EntryPoint {
-
-	FlowPanel flPanel1 = new FlowPanel();
-	FlowPanel flPanel2 = new FlowPanel();
-	HorizontalPanel horizonPanel = new HorizontalPanel();
-	Label lbN1 = new Label("Numero 1");
-	TextBox txtN1 = new TextBox();
-	Label lbN2 = new Label("Numero 2");
-	TextBox txtN2 = new TextBox();
+public class CalculadoraTeste extends botoesNumericos implements EntryPoint  {
+	//Table usada para montar a calculadora
+	FlexTable flTable = new FlexTable();
+	
+	//Funçoes da calculadora
 	Button somar = new Button("+");
 	Button subtrair = new Button("-");
 	Button dividir = new Button("/");
 	Button multiplicar = new Button("*");
-	Label lblResultado = new Label("Resultado"); 
+	Button apagarTudo = new Button("C");
+	Button igual = new Button("=");
+	Button ponto = new Button(".");
+	Button apagarUm = new Button("<=");
+	
+	
+	
+	
+	Double numero1;
+	Double numero2;
+	
+	String sinal;
+	
+	
+	
+	
 	@Override
 	public void onModuleLoad() {
 		// TODO Auto-generated method stub
-	
 		
+		
+		flTable.setWidget(0, 0, apagarTudo);
+		flTable.setWidget(0, 3, dividir);
+		flTable.setWidget(1, 0, sete);
+		flTable.setWidget(1, 1, oito);
+		flTable.setWidget(1, 2, nove);
+		flTable.setWidget(1, 3, multiplicar);
+		flTable.setWidget(2, 0, quatro);
+		flTable.setWidget(2, 1, cinco);
+		flTable.setWidget(2, 2, seis);
+		flTable.setWidget(2, 3, subtrair);
+		flTable.setWidget(3, 0, um);
+		flTable.setWidget(3, 1, dois);
+		flTable.setWidget(3, 2, tres);
+		flTable.setWidget(3, 3, somar);
+		flTable.setWidget(4, 0, zero);
+		flTable.setWidget(4, 1, ponto);
+		//flTable.setWidget(4, 2, apagarUm);
+		flTable.setWidget(4, 3, igual);
+		flTable.setCellPadding(5);
+		
+		// Clique de Botoes Numericos
+		zero.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				txtN1.setText(txtN1.getText() + "0");
+
+			}
+		});
+		um.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				txtN1.setText(txtN1.getText() + "1");
+
+			}
+		});
+		dois.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				txtN1.setText(txtN1.getText() + "2");
+
+			}
+		});
+		tres.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				txtN1.setText(txtN1.getText() + "3");
+
+			}
+		});
+		quatro.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				txtN1.setText(txtN1.getText() + "4");
+
+			}
+		});
+		cinco.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				txtN1.setText(txtN1.getText() + "5");
+
+			}
+		});
+		seis.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				txtN1.setText(txtN1.getText() + "6");
+
+			}
+		});
+		sete.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				txtN1.setText(txtN1.getText() + "7");
+
+			}
+		});
+		oito.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				txtN1.setText(txtN1.getText() + "8");
+
+			}
+		});
+		nove.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				txtN1.setText(txtN1.getText() + "9");
+
+			}
+		});
+		
+		
+		
+		
+		
+		//Botao de apagar 
+		apagarTudo.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				txtN1.setText("");
+				
+			}
+		});
+
+		
+		
+		//funcoes
+		ponto.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				txtN1.setText(txtN1.getText() + ".");
+				
+			}
+		});
 		somar.addClickHandler(new ClickHandler() {
 			
 			@Override
 			public void onClick(ClickEvent event) {
-				// TODO Auto-generated method stub
-				Double n1 = Double.parseDouble(txtN1.getText());
-				Double n2 = Double.parseDouble(txtN2.getText());
-				Double Soma = n1 + n2;
-				lblResultado.setText(Double.toString(Soma));
+			
+				numero1 = Double.parseDouble(txtN1.getText());
+				txtN1.setText("");
+				sinal = "soma";
 			}
 		});
-		
 		subtrair.addClickHandler(new ClickHandler() {
 			
 			@Override
 			public void onClick(ClickEvent event) {
-				Double n1 = Double.parseDouble(txtN1.getText());
-				Double n2 = Double.parseDouble(txtN2.getText());
-				Double Sub = n1 - n2;
-				lblResultado.setText(Double.toString(Sub));
+				numero1 = Double.parseDouble(txtN1.getText());
+				txtN1.setText("");
+				sinal = "subtrair";
 				
 			}
 		});
-		
 		dividir.addClickHandler(new ClickHandler() {
 			
 			@Override
 			public void onClick(ClickEvent event) {
-				Double n1 = Double.parseDouble(txtN1.getText());
-				Double n2 = Double.parseDouble(txtN2.getText());
-				Double Sub = n1 / n2;
-				lblResultado.setText(Double.toString(Sub));
+				numero1 = Double.parseDouble(txtN1.getText());
+				txtN1.setText("");
+				sinal = "dividir";
 				
 			}
 		});
-		
 		multiplicar.addClickHandler(new ClickHandler() {
 			
 			@Override
 			public void onClick(ClickEvent event) {
-				Double n1 = Double.parseDouble(txtN1.getText());
-				Double n2 = Double.parseDouble(txtN2.getText());
-				Double Sub = n1 * n2;
-				lblResultado.setText(Double.toString(Sub));
+				numero1 = Double.parseDouble(txtN1.getText());
+				txtN1.setText("");
+				sinal = "multiplicar";
 				
 			}
 		});
 		
+		//calcula o resultado
+		igual.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				numero2 = Double.parseDouble(txtN1.getText());
+				if (sinal.equals("soma")) {
+					txtN1.setText(Double.toString(numero1 + numero2));
+				}
+				if (sinal.equals("subtrair")) {
+					txtN1.setText(Double.toString(numero1 - numero2));
+				}
+				if (sinal.equals("dividir")) {
+					txtN1.setText(Double.toString(numero1 / numero2));
+				}
+				if (sinal.equals("multiplicar")) {
+					txtN1.setText(Double.toString(numero1 * numero2));
+				}
+			}
+		});
 		
-		flPanel2.setStyleName("icons-hori");
-		//horizonPanel.setSpacing(1);
-		//horizonPanel.setWidth("100%");
-		//horizonPanel.setHeight("100%");
-		//horizonPanel.setHorizontalAlignment(HasAlignment.ALIGN_CENTER);
+	
 		
+		FlowPanel flowPanel = new FlowPanel();
+		flowPanel.add(txtN1);
+		flowPanel.add(flTable);
+		flowPanel.setStyleName("flowpainel");
 		
-		flPanel1.add(lbN1);
-		flPanel1.add(txtN1);
-		flPanel1.add(lbN2);
-		flPanel1.add(txtN2);
-		flPanel1.add(lblResultado);
-		flPanel2.add(somar);
-		flPanel2.add(subtrair);
-		flPanel2.add(dividir);
-		flPanel2.add(multiplicar);
+	
+		RootPanel.get("centro").add(flowPanel);
 		
-		RootPanel.get().add(flPanel1);
-		RootPanel.get().add(flPanel2);
+	
 	}
 	
 	
---
+++ src/com/calc/client/botoesNumericos.java
@@ -0,0 +1,23 @@
+package com.calc.client;
+
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.TextBox;
+
+public class botoesNumericos  {
+
+	TextBox txtN1 = new TextBox();
+	//Numeros usados na calculadora	
+	Button zero = new Button("0");
+	Button um = new Button("1");
+	Button dois = new Button("2");
+	Button tres = new Button("3");
+	Button quatro = new Button("4");
+	Button cinco = new Button("5");
+	Button seis = new Button("6");
+	Button sete = new Button("7");
+	Button oito = new Button("8");
+	Button nove = new Button("9");
+	
+	
+	
+}
